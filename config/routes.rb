c67abc7a963eaf990a023fdc52ff6e838a7e3489
Benah0987 <wanyoikebenayah77@gmail.com>
2023-06-20 16:39:33 +0300

    Assignment Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   get '/cheeses', to: 'cheeses#index'
+
+  # route for cheese
+# get '/cheeses/1', to: "cheeses#first"
+# get '/cheeses/2', to: "cheeses#second"
+# get '/cheeses/3', to: "cheeses#third"\
+
+get '/cheeses/:id', to: 'cheeses#show'
+  
 end
